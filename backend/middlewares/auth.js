216a4d9efb79acc28787d483d9ff78b4ce1dff60
JuanPablo <juaco7720@gmail.com>
2026-05-15 13:43:28 +0100

    proving harcode token

--- backend/middlewares/auth.js
+++ backend/middlewares/auth.js
@@ -1,12 +1,13 @@
 const jwt = require('jsonwebtoken');
 
+// Usamos la misma clave que pusimos en users.js
 const JWT_SECRET = 'desarrollo-secreto-super-seguro-2024';
 
-module.exports = (req, res, next) => {
+const auth = (req, res, next) => {
   const { authorization } = req.headers;
 
   if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(403).send({ message: 'Acceso denegado: No hay token' });
+    return res.status(403).send({ message: 'Acceso denegado: Formato de token incorrecto' });
   }
 
   const token = authorization.replace('Bearer ', '');
@@ -15,11 +16,11 @@ module.exports = (req, res, next) => {
   try {
     payload = jwt.verify(token, JWT_SECRET);
   } catch (err) {
-    return res.status(403).send({ message: 'Acceso denegado: Token inválido' });
+    return res.status(403).send({ message: 'Acceso denegado: Token inválido o clave incorrecta' });
   }
 
   req.user = payload;
   next();
 };
 
-module.exports = auth;
+module.exports = auth; 
